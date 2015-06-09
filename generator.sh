PeterDaveHello/ColorEchoForShell

commit 74aa7ab45f38e04b798ee7e686161b57f1ace6bc

add sh support and use filename ext to recognize

generator.sh
@@ -1,45 +1,56 @@
 #!/usr/bin/env bash
 
-dist=dist/ColorEcho.sh
+dist=dist/ColorEcho
 table=table.txt
 
-echo '#!/usr/bin/env bash' > $dist
-
-for color in `cat $table | awk '{print $1}'`
+for shell in sh bash
 do
-    for light in "" "Light"
+    newDist="$dist.$shell"
+    echo "#!/usr/bin/env $shell" > $newDist
+    for color in `cat $table | awk '{print $1}'`
     do
-        if [ "$light" = "" ]; then
-            code=3
-        else
-            code=9
-        fi
-        for bold in "" "Bold"
+        for light in "" "Light"
         do
-            if [ "$bold" = "" ]; then
-                bCode=
+            if [ "$light" = "" ]; then
+                code=3
             else
-                bCode='1;'
+                code=9
             fi
-            for underLine in "" "UL"
+            for bold in "" "Bold"
             do
-                echo "" >> $dist
-                echo "function echo.$light$bold$underLine$color()" >> $dist
-                if [ "$underLine" = "" ]; then
-                    ulCode=
+                if [ "$bold" = "" ]; then
+                    bCode=
                 else
-                    ulCode='4;'
+                    bCode='1;'
                 fi
-                echo "{" >> $dist
-                echo '    echo -e "\e['"$ulCode$bCode$code"$(grep $color $table | awk '{print $2}')'m$@\e[m"' >> $dist
-                echo "}" >> $dist
+                for underLine in "" "UL"
+                do
+                    echo "" >> $newDist
+                    if [ "$shell" = "bash" ]; then
+                        fn='function '
+                        dot='.'
+                        echo='echo'
+                    else
+                        fn=
+                        dot=
+                        echo='/bin/echo'
+                    fi
+                    echo "$fn echo$dot$light$bold$underLine$color()" >> $newDist
+                    if [ "$underLine" = "" ]; then
+                        ulCode=
+                    else
+                        ulCode='4;'
+                    fi
+                    echo "{" >> $newDist
+                    echo "    $echo"' -e "\e['"$ulCode$bCode$code"$(grep $color $table | awk '{print $2}')'m$@\e[m"' >> $newDist
+                    echo "}" >> $newDist
+                done
             done
         done
     done
-done
 
-cat << LOLCAT >> "$dist"
-function echo.Rainbow()
+cat << LOLCAT >> "$newDist"
+$fn echo$dotRainbow()
 {
     if [ "type lolcat" ]; then
         echo "\$@" | lolcat
@@ -48,3 +59,5 @@ function echo.Rainbow()
     fi
 }
 LOLCAT
+
+done

dist/ColorEcho.bash
@@ -1,0 +1,249 @@
+#!/usr/bin/env bash
+
+function  echo.Red()
+{
+    echo -e "\e[31m$@\e[m"
+}
+
+function  echo.ULRed()
+{
+    echo -e "\e[4;31m$@\e[m"
+}
+
+function  echo.BoldRed()
+{
+    echo -e "\e[1;31m$@\e[m"
+}
+
+function  echo.BoldULRed()
+{
+    echo -e "\e[4;1;31m$@\e[m"
+}
+
+function  echo.LightRed()
+{
+    echo -e "\e[91m$@\e[m"
+}
+
+function  echo.LightULRed()
+{
+    echo -e "\e[4;91m$@\e[m"
+}
+
+function  echo.LightBoldRed()
+{
+    echo -e "\e[1;91m$@\e[m"
+}
+
+function  echo.LightBoldULRed()
+{
+    echo -e "\e[4;1;91m$@\e[m"
+}
+
+function  echo.Green()
+{
+    echo -e "\e[32m$@\e[m"
+}
+
+function  echo.ULGreen()
+{
+    echo -e "\e[4;32m$@\e[m"
+}
+
+function  echo.BoldGreen()
+{
+    echo -e "\e[1;32m$@\e[m"
+}
+
+function  echo.BoldULGreen()
+{
+    echo -e "\e[4;1;32m$@\e[m"
+}
+
+function  echo.LightGreen()
+{
+    echo -e "\e[92m$@\e[m"
+}
+
+function  echo.LightULGreen()
+{
+    echo -e "\e[4;92m$@\e[m"
+}
+
+function  echo.LightBoldGreen()
+{
+    echo -e "\e[1;92m$@\e[m"
+}
+
+function  echo.LightBoldULGreen()
+{
+    echo -e "\e[4;1;92m$@\e[m"
+}
+
+function  echo.Yellow()
+{
+    echo -e "\e[33m$@\e[m"
+}
+
+function  echo.ULYellow()
+{
+    echo -e "\e[4;33m$@\e[m"
+}
+
+function  echo.BoldYellow()
+{
+    echo -e "\e[1;33m$@\e[m"
+}
+
+function  echo.BoldULYellow()
+{
+    echo -e "\e[4;1;33m$@\e[m"
+}
+
+function  echo.LightYellow()
+{
+    echo -e "\e[93m$@\e[m"
+}
+
+function  echo.LightULYellow()
+{
+    echo -e "\e[4;93m$@\e[m"
+}
+
+function  echo.LightBoldYellow()
+{
+    echo -e "\e[1;93m$@\e[m"
+}
+
+function  echo.LightBoldULYellow()
+{
+    echo -e "\e[4;1;93m$@\e[m"
+}
+
+function  echo.Blue()
+{
+    echo -e "\e[34m$@\e[m"
+}
+
+function  echo.ULBlue()
+{
+    echo -e "\e[4;34m$@\e[m"
+}
+
+function  echo.BoldBlue()
+{
+    echo -e "\e[1;34m$@\e[m"
+}
+
+function  echo.BoldULBlue()
+{
+    echo -e "\e[4;1;34m$@\e[m"
+}
+
+function  echo.LightBlue()
+{
+    echo -e "\e[94m$@\e[m"
+}
+
+function  echo.LightULBlue()
+{
+    echo -e "\e[4;94m$@\e[m"
+}
+
+function  echo.LightBoldBlue()
+{
+    echo -e "\e[1;94m$@\e[m"
+}
+
+function  echo.LightBoldULBlue()
+{
+    echo -e "\e[4;1;94m$@\e[m"
+}
+
+function  echo.Magenta()
+{
+    echo -e "\e[35m$@\e[m"
+}
+
+function  echo.ULMagenta()
+{
+    echo -e "\e[4;35m$@\e[m"
+}
+
+function  echo.BoldMagenta()
+{
+    echo -e "\e[1;35m$@\e[m"
+}
+
+function  echo.BoldULMagenta()
+{
+    echo -e "\e[4;1;35m$@\e[m"
+}
+
+function  echo.LightMagenta()
+{
+    echo -e "\e[95m$@\e[m"
+}
+
+function  echo.LightULMagenta()
+{
+    echo -e "\e[4;95m$@\e[m"
+}
+
+function  echo.LightBoldMagenta()
+{
+    echo -e "\e[1;95m$@\e[m"
+}
+
+function  echo.LightBoldULMagenta()
+{
+    echo -e "\e[4;1;95m$@\e[m"
+}
+
+function  echo.Cyan()
+{
+    echo -e "\e[36m$@\e[m"
+}
+
+function  echo.ULCyan()
+{
+    echo -e "\e[4;36m$@\e[m"
+}
+
+function  echo.BoldCyan()
+{
+    echo -e "\e[1;36m$@\e[m"
+}
+
+function  echo.BoldULCyan()
+{
+    echo -e "\e[4;1;36m$@\e[m"
+}
+
+function  echo.LightCyan()
+{
+    echo -e "\e[96m$@\e[m"
+}
+
+function  echo.LightULCyan()
+{
+    echo -e "\e[4;96m$@\e[m"
+}
+
+function  echo.LightBoldCyan()
+{
+    echo -e "\e[1;96m$@\e[m"
+}
+
+function  echo.LightBoldULCyan()
+{
+    echo -e "\e[4;1;96m$@\e[m"
+}
+function  echo()
+{
+    if [ "type lolcat" ]; then
+        echo "$@" | lolcat
+    else
+        echo "$@"
+    fi
+}

dist/ColorEcho.sh
@@ -1,245 +1,245 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-function echo.Red()
+ echoRed()
 {
-    echo -e "\e[31m$@\e[m"
+    /bin/echo -e "\e[31m$@\e[m"
 }
 
-function echo.ULRed()
+ echoULRed()
 {
-    echo -e "\e[4;31m$@\e[m"
+    /bin/echo -e "\e[4;31m$@\e[m"
 }
 
-function echo.BoldRed()
+ echoBoldRed()
 {
-    echo -e "\e[1;31m$@\e[m"
+    /bin/echo -e "\e[1;31m$@\e[m"
 }
 
-function echo.BoldULRed()
+ echoBoldULRed()
 {
-    echo -e "\e[4;1;31m$@\e[m"
+    /bin/echo -e "\e[4;1;31m$@\e[m"
 }
 
-function echo.LightRed()
+ echoLightRed()
 {
-    echo -e "\e[91m$@\e[m"
+    /bin/echo -e "\e[91m$@\e[m"
 }
 
-function echo.LightULRed()
+ echoLightULRed()
 {
-    echo -e "\e[4;91m$@\e[m"
+    /bin/echo -e "\e[4;91m$@\e[m"
 }
 
-function echo.LightBoldRed()
+ echoLightBoldRed()
 {
-    echo -e "\e[1;91m$@\e[m"
+    /bin/echo -e "\e[1;91m$@\e[m"
 }
 
-function echo.LightBoldULRed()
+ echoLightBoldULRed()
 {
-    echo -e "\e[4;1;91m$@\e[m"
+    /bin/echo -e "\e[4;1;91m$@\e[m"
 }
 
-function echo.Green()
+ echoGreen()
 {
-    echo -e "\e[32m$@\e[m"
+    /bin/echo -e "\e[32m$@\e[m"
 }
 
-function echo.ULGreen()
+ echoULGreen()
 {
-    echo -e "\e[4;32m$@\e[m"
+    /bin/echo -e "\e[4;32m$@\e[m"
 }
 
-function echo.BoldGreen()
+ echoBoldGreen()
 {
-    echo -e "\e[1;32m$@\e[m"
+    /bin/echo -e "\e[1;32m$@\e[m"
 }
 
-function echo.BoldULGreen()
+ echoBoldULGreen()
 {
-    echo -e "\e[4;1;32m$@\e[m"
+    /bin/echo -e "\e[4;1;32m$@\e[m"
 }
 
-function echo.LightGreen()
+ echoLightGreen()
 {
-    echo -e "\e[92m$@\e[m"
+    /bin/echo -e "\e[92m$@\e[m"
 }
 
-function echo.LightULGreen()
+ echoLightULGreen()
 {
-    echo -e "\e[4;92m$@\e[m"
+    /bin/echo -e "\e[4;92m$@\e[m"
 }
 
-function echo.LightBoldGreen()
+ echoLightBoldGreen()
 {
-    echo -e "\e[1;92m$@\e[m"
+    /bin/echo -e "\e[1;92m$@\e[m"
 }
 
-function echo.LightBoldULGreen()
+ echoLightBoldULGreen()
 {
-    echo -e "\e[4;1;92m$@\e[m"
+    /bin/echo -e "\e[4;1;92m$@\e[m"
 }
 
-function echo.Yellow()
+ echoYellow()
 {
-    echo -e "\e[33m$@\e[m"
+    /bin/echo -e "\e[33m$@\e[m"
 }
 
-function echo.ULYellow()
+ echoULYellow()
 {
-    echo -e "\e[4;33m$@\e[m"
+    /bin/echo -e "\e[4;33m$@\e[m"
 }
 
-function echo.BoldYellow()
+ echoBoldYellow()
 {
-    echo -e "\e[1;33m$@\e[m"
+    /bin/echo -e "\e[1;33m$@\e[m"
 }
 
-function echo.BoldULYellow()
+ echoBoldULYellow()
 {
-    echo -e "\e[4;1;33m$@\e[m"
+    /bin/echo -e "\e[4;1;33m$@\e[m"
 }
 
-function echo.LightYellow()
+ echoLightYellow()
 {
-    echo -e "\e[93m$@\e[m"
+    /bin/echo -e "\e[93m$@\e[m"
 }
 
-function echo.LightULYellow()
+ echoLightULYellow()
 {
-    echo -e "\e[4;93m$@\e[m"
+    /bin/echo -e "\e[4;93m$@\e[m"
 }
 
-function echo.LightBoldYellow()
+ echoLightBoldYellow()
 {
-    echo -e "\e[1;93m$@\e[m"
+    /bin/echo -e "\e[1;93m$@\e[m"
 }
 
-function echo.LightBoldULYellow()
+ echoLightBoldULYellow()
 {
-    echo -e "\e[4;1;93m$@\e[m"
+    /bin/echo -e "\e[4;1;93m$@\e[m"
 }
 
-function echo.Blue()
+ echoBlue()
 {
-    echo -e "\e[34m$@\e[m"
+    /bin/echo -e "\e[34m$@\e[m"
 }
 
-function echo.ULBlue()
+ echoULBlue()
 {
-    echo -e "\e[4;34m$@\e[m"
+    /bin/echo -e "\e[4;34m$@\e[m"
 }
 
-function echo.BoldBlue()
+ echoBoldBlue()
 {
-    echo -e "\e[1;34m$@\e[m"
+    /bin/echo -e "\e[1;34m$@\e[m"
 }
 
-function echo.BoldULBlue()
+ echoBoldULBlue()
 {
-    echo -e "\e[4;1;34m$@\e[m"
+    /bin/echo -e "\e[4;1;34m$@\e[m"
 }
 
-function echo.LightBlue()
+ echoLightBlue()
 {
-    echo -e "\e[94m$@\e[m"
+    /bin/echo -e "\e[94m$@\e[m"
 }
 
-function echo.LightULBlue()
+ echoLightULBlue()
 {
-    echo -e "\e[4;94m$@\e[m"
+    /bin/echo -e "\e[4;94m$@\e[m"
 }
 
-function echo.LightBoldBlue()
+ echoLightBoldBlue()
 {
-    echo -e "\e[1;94m$@\e[m"
+    /bin/echo -e "\e[1;94m$@\e[m"
 }
 
-function echo.LightBoldULBlue()
+ echoLightBoldULBlue()
 {
-    echo -e "\e[4;1;94m$@\e[m"
+    /bin/echo -e "\e[4;1;94m$@\e[m"
 }
 
-function echo.Magenta()
+ echoMagenta()
 {
-    echo -e "\e[35m$@\e[m"
+    /bin/echo -e "\e[35m$@\e[m"
 }
 
-function echo.ULMagenta()
+ echoULMagenta()
 {
-    echo -e "\e[4;35m$@\e[m"
+    /bin/echo -e "\e[4;35m$@\e[m"
 }
 
-function echo.BoldMagenta()
+ echoBoldMagenta()
 {
-    echo -e "\e[1;35m$@\e[m"
+    /bin/echo -e "\e[1;35m$@\e[m"
 }
 
-function echo.BoldULMagenta()
+ echoBoldULMagenta()
 {
-    echo -e "\e[4;1;35m$@\e[m"
+    /bin/echo -e "\e[4;1;35m$@\e[m"
 }
 
-function echo.LightMagenta()
+ echoLightMagenta()
 {
-    echo -e "\e[95m$@\e[m"
+    /bin/echo -e "\e[95m$@\e[m"
 }
 
-function echo.LightULMagenta()
+ echoLightULMagenta()
 {
-    echo -e "\e[4;95m$@\e[m"
+    /bin/echo -e "\e[4;95m$@\e[m"
 }
 
-function echo.LightBoldMagenta()
+ echoLightBoldMagenta()
 {
-    echo -e "\e[1;95m$@\e[m"
+    /bin/echo -e "\e[1;95m$@\e[m"
 }
 
-function echo.LightBoldULMagenta()
+ echoLightBoldULMagenta()
 {
-    echo -e "\e[4;1;95m$@\e[m"
+    /bin/echo -e "\e[4;1;95m$@\e[m"
 }
 
-function echo.Cyan()
+ echoCyan()
 {
-    echo -e "\e[36m$@\e[m"
+    /bin/echo -e "\e[36m$@\e[m"
 }
 
-function echo.ULCyan()
+ echoULCyan()
 {
-    echo -e "\e[4;36m$@\e[m"
+    /bin/echo -e "\e[4;36m$@\e[m"
 }
 
-function echo.BoldCyan()
+ echoBoldCyan()
 {
-    echo -e "\e[1;36m$@\e[m"
+    /bin/echo -e "\e[1;36m$@\e[m"
 }
 
-function echo.BoldULCyan()
+ echoBoldULCyan()
 {
-    echo -e "\e[4;1;36m$@\e[m"
+    /bin/echo -e "\e[4;1;36m$@\e[m"
 }
 
-function echo.LightCyan()
+ echoLightCyan()
 {
-    echo -e "\e[96m$@\e[m"
+    /bin/echo -e "\e[96m$@\e[m"
 }
 
-function echo.LightULCyan()
+ echoLightULCyan()
 {
-    echo -e "\e[4;96m$@\e[m"
+    /bin/echo -e "\e[4;96m$@\e[m"
 }
 
-function echo.LightBoldCyan()
+ echoLightBoldCyan()
 {
-    echo -e "\e[1;96m$@\e[m"
+    /bin/echo -e "\e[1;96m$@\e[m"
 }
 
-function echo.LightBoldULCyan()
+ echoLightBoldULCyan()
 {
-    echo -e "\e[4;1;96m$@\e[m"
+    /bin/echo -e "\e[4;1;96m$@\e[m"
 }
-function echo.Rainbow()
+ echo()
 {
     if [ "type lolcat" ]; then
         echo "$@" | lolcat

dist/ColorEcho.zsh
@@ -1,0 +1,249 @@
+#!/usr/bin/env bash
+
+function  echo.Red()
+{
+    echo -e "\e[31m$@\e[m"
+}
+
+function  echo.ULRed()
+{
+    echo -e "\e[4;31m$@\e[m"
+}
+
+function  echo.BoldRed()
+{
+    echo -e "\e[1;31m$@\e[m"
+}
+
+function  echo.BoldULRed()
+{
+    echo -e "\e[4;1;31m$@\e[m"
+}
+
+function  echo.LightRed()
+{
+    echo -e "\e[91m$@\e[m"
+}
+
+function  echo.LightULRed()
+{
+    echo -e "\e[4;91m$@\e[m"
+}
+
+function  echo.LightBoldRed()
+{
+    echo -e "\e[1;91m$@\e[m"
+}
+
+function  echo.LightBoldULRed()
+{
+    echo -e "\e[4;1;91m$@\e[m"
+}
+
+function  echo.Green()
+{
+    echo -e "\e[32m$@\e[m"
+}
+
+function  echo.ULGreen()
+{
+    echo -e "\e[4;32m$@\e[m"
+}
+
+function  echo.BoldGreen()
+{
+    echo -e "\e[1;32m$@\e[m"
+}
+
+function  echo.BoldULGreen()
+{
+    echo -e "\e[4;1;32m$@\e[m"
+}
+
+function  echo.LightGreen()
+{
+    echo -e "\e[92m$@\e[m"
+}
+
+function  echo.LightULGreen()
+{
+    echo -e "\e[4;92m$@\e[m"
+}
+
+function  echo.LightBoldGreen()
+{
+    echo -e "\e[1;92m$@\e[m"
+}
+
+function  echo.LightBoldULGreen()
+{
+    echo -e "\e[4;1;92m$@\e[m"
+}
+
+function  echo.Yellow()
+{
+    echo -e "\e[33m$@\e[m"
+}
+
+function  echo.ULYellow()
+{
+    echo -e "\e[4;33m$@\e[m"
+}
+
+function  echo.BoldYellow()
+{
+    echo -e "\e[1;33m$@\e[m"
+}
+
+function  echo.BoldULYellow()
+{
+    echo -e "\e[4;1;33m$@\e[m"
+}
+
+function  echo.LightYellow()
+{
+    echo -e "\e[93m$@\e[m"
+}
+
+function  echo.LightULYellow()
+{
+    echo -e "\e[4;93m$@\e[m"
+}
+
+function  echo.LightBoldYellow()
+{
+    echo -e "\e[1;93m$@\e[m"
+}
+
+function  echo.LightBoldULYellow()
+{
+    echo -e "\e[4;1;93m$@\e[m"
+}
+
+function  echo.Blue()
+{
+    echo -e "\e[34m$@\e[m"
+}
+
+function  echo.ULBlue()
+{
+    echo -e "\e[4;34m$@\e[m"
+}
+
+function  echo.BoldBlue()
+{
+    echo -e "\e[1;34m$@\e[m"
+}
+
+function  echo.BoldULBlue()
+{
+    echo -e "\e[4;1;34m$@\e[m"
+}
+
+function  echo.LightBlue()
+{
+    echo -e "\e[94m$@\e[m"
+}
+
+function  echo.LightULBlue()
+{
+    echo -e "\e[4;94m$@\e[m"
+}
+
+function  echo.LightBoldBlue()
+{
+    echo -e "\e[1;94m$@\e[m"
+}
+
+function  echo.LightBoldULBlue()
+{
+    echo -e "\e[4;1;94m$@\e[m"
+}
+
+function  echo.Magenta()
+{
+    echo -e "\e[35m$@\e[m"
+}
+
+function  echo.ULMagenta()
+{
+    echo -e "\e[4;35m$@\e[m"
+}
+
+function  echo.BoldMagenta()
+{
+    echo -e "\e[1;35m$@\e[m"
+}
+
+function  echo.BoldULMagenta()
+{
+    echo -e "\e[4;1;35m$@\e[m"
+}
+
+function  echo.LightMagenta()
+{
+    echo -e "\e[95m$@\e[m"
+}
+
+function  echo.LightULMagenta()
+{
+    echo -e "\e[4;95m$@\e[m"
+}
+
+function  echo.LightBoldMagenta()
+{
+    echo -e "\e[1;95m$@\e[m"
+}
+
+function  echo.LightBoldULMagenta()
+{
+    echo -e "\e[4;1;95m$@\e[m"
+}
+
+function  echo.Cyan()
+{
+    echo -e "\e[36m$@\e[m"
+}
+
+function  echo.ULCyan()
+{
+    echo -e "\e[4;36m$@\e[m"
+}
+
+function  echo.BoldCyan()
+{
+    echo -e "\e[1;36m$@\e[m"
+}
+
+function  echo.BoldULCyan()
+{
+    echo -e "\e[4;1;36m$@\e[m"
+}
+
+function  echo.LightCyan()
+{
+    echo -e "\e[96m$@\e[m"
+}
+
+function  echo.LightULCyan()
+{
+    echo -e "\e[4;96m$@\e[m"
+}
+
+function  echo.LightBoldCyan()
+{
+    echo -e "\e[1;96m$@\e[m"
+}
+
+function  echo.LightBoldULCyan()
+{
+    echo -e "\e[4;1;96m$@\e[m"
+}
+function  echo()
+{
+    if [ "type lolcat" ]; then
+        echo "$@" | lolcat
+    else
+        echo "$@"
+    fi
+}

commit 14fbe9d4133aa6c5742f76019ffd7d0667f54053

fix bug in rainbow, caused by 74aa7ab

generator.sh
@@ -49,8 +49,8 @@ do
         done
     done
 
+echo "$fn echo"$dot"Rainbow()" >> "$newDist"
 cat << LOLCAT >> "$newDist"
-$fn echo$dotRainbow()
 {
     if [ "type lolcat" ]; then
         echo "\$@" | lolcat